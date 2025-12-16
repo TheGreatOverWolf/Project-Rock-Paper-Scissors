cc8ebdf3a4370e2c6f70c271fe675d3ecae8ef9e
TheGreatOverWolf <96438404+TheGreatOverWolf@users.noreply.github.com>
2025-12-16 15:25:31 +0100

    bug fix

--- js/script.js
+++ js/script.js
@@ -1,20 +1,19 @@
 //crate var for player input
-
-
-
 //crate function getComputerChoice
-function getComputerChoice(){
 //function genert random number 1-3
-    let choice = Math.floor(Math.random(3));
 //save the number in var choice 
-
 //crate if stetment number is 1 take rock
-    if (choice = 1 ) {
-        console.log("Rock")
 //else if 2 take paper
-    } else if (choice = 2 ) {
-        console.log("Paper")
 //else scissors
-    } else (choice = 3 )
-        console.log("scissors")
-}
+
+
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3) + 1; 
+    if (choice === 1) {
+        console.log("Rock";)
+    } else if (choice === 2) {
+        console.log("Paper");
+    } else {  
+        console.log("Scissors");
+    }
+    }
